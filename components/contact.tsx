1d737c113079f4c9719d72578ac267d446b47071
Ankit Kr Chowdhury <rakesh856100@gmail.com>
2025-03-14 03:02:49 +0530

    Refactor contact component: remove CSS file, enhance structure for improved responsiveness, and update styling for better accessibility

--- components/contact.tsx
+++ components/contact.tsx
@@ -30,15 +30,36 @@ export default function Contact() {
   }, []);
 
   return (
-    <section className='contact_container' ref={contactRef}>
-      <h4>05.</h4>
-      <h1>Get In Touch</h1>
-      <p>
+    <section
+      ref={contactRef}
+      className='contact_container h-[calc(100dvh-169.98px)] sm:h-[calc(100dvh-109.99px)] px-5 sm:px-12 md:px-24 relative z-[2] mx-auto flex flex-col items-center justify-center'
+    >
+      <h4 className='font-mono text-base sm:text-lg md:text-xl lg:text-2xl font-light text-primary'>
+        05.
+      </h4>
+
+      <h1
+        style={{ wordSpacing: '5px' }}
+        className='mt-2 font-noto text-[clamp(30px,5vw,45px)] lg:text-[45px] text-heading font-semibold tracking-[1px]'
+      >
+        Get In Touch
+      </h1>
+
+      <p
+        style={{ wordSpacing: '0.8px' }}
+        className='max-w-[550px] mt-4 tracking-[0.4px] text-sm sm:text-base font-source text-center text-para'
+      >
         Feel free to reach out to me through the contact button below. I&apos;m
         excited to connect and discuss any web development opportunities or
         projects you have in mind. Let&apos;s build something great together!
       </p>
-      <Link href='mailto:ankitparallax@gmail.com'>Let&apos;s Connect</Link>
+
+      <Link
+        href='mailto:ankitparallax@gmail.com'
+        className='font-mono text-primary text-sm sm:text-base font-light bg-transparent rounded-sm border border-primary py-3 px-6 mt-9 mx-auto hover:bg-hover tracking-[1px] transition-colors'
+      >
+        Let&apos;s Connect
+      </Link>
     </section>
   );
 }
