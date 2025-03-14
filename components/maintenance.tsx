2d5d53d24ce49c6b318fb8a87284d6a56a3e7520
Ankit Kr Chowdhury <rakesh856100@gmail.com>
2025-03-14 15:47:33 +0530

    Update maintenance mode functionality and enhance maintenance component styling; remove unused CSS file

--- components/maintenance.tsx
+++ components/maintenance.tsx
@@ -1,10 +1,11 @@
-import '../styles/maintenance.css';
-
 export default function Maintenance() {
   return (
-    <section className='maintenance_container'>
-      <h1>Under Maintenance</h1>
-      <p>
+    <section className='h-dvh flex flex-col justify-center items-center gap-y-3 sm:gap-y-6 px-5'>
+      <h1 className='text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-heading font-noto font-semibold'>
+        Under Maintenance
+      </h1>
+
+      <p className='text-para text-center font-source text-sm sm:text-base'>
         Sorry for the inconvenience. I am currently updating my portfolio.
         Please check back later.
       </p>
