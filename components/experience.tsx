237b6f96ec2ca2f595489b16811bbc30fea4988e
Ankit Kr Chowdhury <rakesh856100@gmail.com>
2025-03-22 16:16:37 +0530

    Update experience data structure to include location and employment type for better clarity

--- components/experience.tsx
+++ components/experience.tsx
@@ -155,7 +155,8 @@ export default function Experience() {
           </h2>
 
           <p className='font-open-sans text-para tracking-[1.6px] font-medium whitespace-nowrap text-xs'>
-            {selectedExperience.date}
+            {selectedExperience.location}| {selectedExperience.date} |{' '}
+            {selectedExperience.employmentType}
           </p>
 
           <ul className='flex flex-col gap-y-4 mt-3 experience-item'>
