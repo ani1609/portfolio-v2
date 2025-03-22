bee5d5bb6d33e3a0bd2bf861b3d1767e00df4875
Ankit Kr Chowdhury <rakesh856100@gmail.com>
2025-03-22 23:58:14 +0530

    Enhance Section component by adding className prop for improved styling flexibility

--- components/ui/section.tsx
+++ components/ui/section.tsx
@@ -1,15 +1,17 @@
 import React, { ReactNode, forwardRef } from 'react';
 import { Section as SectionType } from '@/types/nav';
+import { cn } from '@/lib/utils';
 
 interface SectionProps extends React.HTMLAttributes<HTMLElement> {
   id: SectionType;
+  className?: string;
   children: ReactNode;
 }
 
 const Section = forwardRef<HTMLElement, SectionProps>(
-  ({ id, children, ...props }, ref) => {
+  ({ id, className, children, ...props }, ref) => {
     return (
-      <section id={id} ref={ref} {...props}>
+      <section id={id} ref={ref} className={cn('', className)} {...props}>
         {children}
       </section>
     );
