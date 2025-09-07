a44178d20e38a2ed74a3bcd6c00285f01e2c7c06
Ankit Kr. Chowdhury <rakesh856100@gmail.com>
2025-09-08 03:52:21 +0530

    Update resume link to the latest version

--- data/header.ts
+++ data/header.ts
@@ -11,6 +11,6 @@ export const navItems: NavItem[] = [
 ];
 
 export const resumeLink: string =
-  'https://drive.google.com/file/d/1YTaKaaGpWHLNf1vitduTPmzSq1VRwOb4/view?usp=sharing';
+  'https://drive.google.com/file/d/1BDJoiYXhJj8odv7Wf6WD3FujwKWheumc/view?usp=sharing';
 
 export const emailAddress: string = 'ankitparallax@gmail.com';
